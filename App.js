55058bf07066119490625952847c5cbd80066342
hrupesh <rcashok0@gmail.com>
2020-07-19 05:05:33 +0530

    working on FrontEnd

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tracker</Text>
+      <Text>Tracker!</Text>
       <StatusBar style="auto" />
     </View>
   );
