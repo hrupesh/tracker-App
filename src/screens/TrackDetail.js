cfb77b7abeefd9ad4de5a37329b7f95dc5f3a172
hrupesh <rcashok0@gmail.com>
2020-07-20 02:13:30 +0530

    working on FrontEnd

--- src/screens/TrackDetail.js
+++ src/screens/TrackDetail.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function TrackDetail() {
-    return (
-        <View>
-            <Text>TrackDetail Screen</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>TrackDetail Screen</Text>
+      <Button
+        title="Track  List"
+        color="dodgerblue"
+        onPress={() => navigation.navigate("TrackList")}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/TrackList.js
+++ src/screens/TrackList.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function TrackList() {
+export default function TrackList({ navigation }) {
   return (
     <View>
       <Text>TrackList Screen</Text>
