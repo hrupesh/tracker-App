79510f14420a6b252337ed105ce83a2ab7812a5f
hrupesh <rcashok0@gmail.com>
2020-07-25 20:24:27 +0530

    working on FrontEnd

--- src/screens/loadingScreen.js
+++ src/screens/loadingScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, StatusBar } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 
 export default function loadingScreen() {
@@ -11,6 +11,7 @@ export default function loadingScreen() {
 
   return (
     <View style={styles.container}>
+        <StatusBar  backgroundColor="#512DF8" />
       <Image
         style={styles.loader}
         source={{
@@ -18,8 +19,6 @@ export default function loadingScreen() {
             "https://cdn.dribbble.com/users/1162077/screenshots/4382009/animated-pattern.gif",
         }}
         resizeMode="cover"
-        resizeMethod="resize"
-
       />
     </View>
   );
@@ -31,7 +30,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ccc",
   },
   loader: {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
   },
 });
