fd669c8d866417c9ba43de9a752fae56cbe3e029
hrupesh <rcashok0@gmail.com>
2020-07-25 22:25:39 +0530

    working on FrontEnd

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import { Button, Text } from "react-native-elements";
 
 export default function AccountScreen() {
   return (
     <View style={styles.container}>
-      <Text h1>Account Screen</Text>
+      <StatusBar backgroundColor="#27008F" />
+      <Text h1 h1Style={{ color: "#fff", letterSpacing: 1, fontWeight: "100" }}>
+        Account Screen
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#ccc",
+    backgroundColor: "#27008F",
     flex: 1,
+    padding: 25,
+    justifyContent: "space-between",
   },
 });
