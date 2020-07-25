936b96ce4e402b6c5fb4fc044f06fedfb87da3a3
hrupesh <rcashok0@gmail.com>
2020-07-25 22:18:53 +0530

    working on FrontEnd

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Text } from "react-native-elements";
 
 export default function AccountScreen() {
-    return (
-        <View>
-            <Text>Account Screen</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text h1>Account Screen</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ccc",
+    flex: 1,
+  },
+});
