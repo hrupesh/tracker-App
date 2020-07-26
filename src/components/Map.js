43318fe65ec6226000063dc0148b5110d612102e
hrupesh <rcashok0@gmail.com>
2020-07-27 02:05:34 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function Map() {
-  return <MapView />;
+  return <MapView   />;
 }
 
 const styles = StyleSheet.create({});
