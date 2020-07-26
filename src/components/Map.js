8de2c34940106dec4d1a749904dafe5ebab38df7
hrupesh <rcashok0@gmail.com>
2020-07-27 02:07:51 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function Map() {
-  return <MapView   />;
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({});
