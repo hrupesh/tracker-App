77f6ae7adf8fecc5c5e02928b7386c6de908890b
hrupesh <rcashok0@gmail.com>
2020-07-27 02:19:41 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,17 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function Map() {
-  return (
-    <MapView
-    style={{ width:'100%',height:'100%' }}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  );
+  return <MapView style={{ width: "100%", height: "100%" }} />;
 }
 
 const styles = StyleSheet.create({});
