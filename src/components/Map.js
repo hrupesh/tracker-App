7b36bff6699510e8432341db573897c9a619c5cb
hrupesh <rcashok0@gmail.com>
2020-07-27 01:54:26 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Map() {
-    return (
-        <View>
-            <Text>Map Component works!</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>Map Component works!</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
