816cc18ec8fab1e1ec58fad9e060cbab06e116c2
hrupesh <rcashok0@gmail.com>
2020-07-27 16:17:58 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 export default function Map() {
   return <MapView style={{ width: "100%", height: "100%" }} />;
