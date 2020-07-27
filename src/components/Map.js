e3cfc9f7e5dbed7529acd68c6c18a49975a296fa
hrupesh <rcashok0@gmail.com>
2020-07-27 16:21:56 +0530

    working on FrontEnd

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
 
 export default function Map() {
-  return <MapView style={{ width: "100%", height: "100%" }} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 19.98848,
+        longitude: 73.7804288,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      }}
+    >
+      {/* <Polyline /> */}
+    </MapView>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
