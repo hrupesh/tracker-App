8acb21aa0b403cb6440e52c9c04d34aff56ebb38
hrupesh <rcashok0@gmail.com>
2020-07-28 17:03:22 +0530

    Working on RNMaps

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -2,4 +2,17 @@ import * as Location from "expo-location";
 
 const tenMeters = 0.0001;
 
-const 
+const getLocation = (increment) => {
+  return {
+    timestamp: 1000000000,
+    coords: {
+      speed: 0,
+      heading: 0,
+      accuracy: 5,
+      altitudeAccuracy: 5,
+      altitude: 5,
+      latitude: 20.0380938 + increment * tenMeters,
+      longitude: 73.80659 + increment * tenMeters,
+    },
+  };
+};
