a49429c43f5532381bc778d9d36a9da0123ee9c7
hrupesh <rcashok0@gmail.com>
2020-07-28 16:02:28 +0530

    Working on RNMaps

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,10 +5,17 @@ import MapView, { Polyline } from "react-native-maps";
 export default function Map() {
   let points = [];
   for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 20.0380938 + i * 0.0001,
-      longitude: 73.80659 + i * 0.0001,
-    });
+    if( i % 2 === 0){
+      points.push({
+        latitude: 20.0380938 + i * 0.0001,
+        longitude: 73.80659 + i * 0.0001,
+      });
+    }else{
+        points.push({
+          latitude: 20.0380938 - i * 0.0001,
+          longitude: 73.80659 + i * 0.0001,
+        });
+    }
   }
 
   return (
@@ -17,14 +24,13 @@ export default function Map() {
       initialRegion={{
         latitude: 20.0381938,
         longitude: 73.80659,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
       }}
     >
       <Polyline
         coordinates={points}
         strokeColor="#212121"
-        onPress={() => alert("Map !")}
       />
     </MapView>
   );
