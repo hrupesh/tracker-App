b9469fb21121e1ca4365b12c5e2d02a98a1d65ee
hrupesh <rcashok0@gmail.com>
2020-07-30 01:34:18 +0530

    Auto Commit ≡ƒÿÄ

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,1 +1,33 @@
- 
+import { useState, useEffect } from "react";
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
+} from "expo-location";
+
+export default () => {
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 100,
+          distanceInterval: 2,
+        },
+        (location) => {
+          addLocation(location);
+          // console.log(location);
+        }
+      );
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+};
