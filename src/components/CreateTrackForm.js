38b5ce79d88c683fb22179c35c4799edd54b39d5
hrupesh <rcashok0@gmail.com>
2020-07-30 15:39:22 +0530

    Auto Commit ≡ƒÿÄ

--- src/components/CreateTrackForm.js
+++ src/components/CreateTrackForm.js
@@ -5,7 +5,11 @@ import { TextInput } from "react-native-gesture-handler";
 export default function CreateTrackForm() {
   return (
     <View style={styles.formContainer}>
-      <TextInput style={styles.input} placeholder="Enter Track Name Here...." />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Track Name Here...."
+        placeholderTextColor="#B2EBF2"
+      />
     </View>
   );
 }
@@ -14,9 +18,12 @@ const styles = StyleSheet.create({
   formContainer: {
     marginHorizontal: 15,
   },
-  input:{
-      height:40,
-      fontSize:20,
-      padding:5
-  }
+  input: {
+    height: 40,
+    fontSize: 20,
+    padding: 5,
+    borderBottomColor: "#0097A7",
+    borderBottomWidth: 1,
+    color: "#0097A7",
+  },
 });
