4cd2129a1e1cbbf6362e3deb7fea063d1e854a01
hrupesh <rcashok0@gmail.com>
2020-08-05 00:32:32 +0530

    Auto Commit ≡ƒÿÄ

--- src/api/trackapi.js
+++ src/api/trackapi.js
@@ -1,5 +1,20 @@
 import axios from "axios";
+import { AsyncStorage } from "react-native";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "https://traker-api.herokuapp.com",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
