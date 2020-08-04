e399c75db7239974631a87ac96cb76a64c818a72
hrupesh <rcashok0@gmail.com>
2020-08-04 23:45:44 +0530

    Auto Commit ≡ƒÿÄ

---
+++ src/hooks/useSaveTrack.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { Context as LocationContext } from "../context/LocationContext";
+import { Context as TrackContext } from "../context/TrackContext";
+
+export default () => {
+  const { createTrack } = useContext(TrackContext);
+  const {
+    state: { locations, name },
+  } = useContext(LocationContext);
+};
