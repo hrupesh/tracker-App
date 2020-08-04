f222e9ee0cf46fd3d9d25c0c7655264c4b168c5b
hrupesh <rcashok0@gmail.com>
2020-08-04 22:49:15 +0530

    Auto Commit ≡ƒÿÄ

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import TrackList from "./src/screens/TrackList";
 import TrackDetail from "./src/screens/TrackDetail";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 import { Provider as LocationProvider } from "./src/context/LocationContext";
+import { Provider as TrackProvider } from "./src/context/TrackContext";
 import { setNavigator } from "./src/navigationRef";
 import LoadingScreen from "./src/screens/LoadingScreen";
 import { StatusBar } from "react-native";
@@ -34,15 +35,17 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <LocationProvider>
-      <AuthProvider>
-        <StatusBar backgroundColor="#1d2c4dff" animated={true} />
-        <App
-          ref={(navigator) => {
-            setNavigator(navigator);
-          }}
-        />
-      </AuthProvider>
-    </LocationProvider>
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <StatusBar backgroundColor="#1d2c4dff" animated={true} />
+          <App
+            ref={(navigator) => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
   );
 };
