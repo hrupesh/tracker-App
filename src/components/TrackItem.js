a54c31711cec479dd80969cf8b154051af6645c8
hrupesh <rcashok0@gmail.com>
2020-08-07 05:26:01 +0530

    Auto Commit ≡ƒÿÄ

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function TrackItem() {
-    return (
-        <View>
-            <Text>  Track Item Works! </Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text> Track Item Works! </Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
