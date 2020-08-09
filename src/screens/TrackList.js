f773ad2005b7574d13cd9a7c5a1062cc05227019
hrupesh <rcashok0@gmail.com>
2020-08-09 21:38:07 +0530

    Auto Commit 😎

--- src/screens/TrackList.js
+++ src/screens/TrackList.js
@@ -58,7 +58,7 @@ export default function TrackList({ navigation }) {
         />
       ) : null}
       {state.tracks.length < 1 && !loading ? (
-        <View>
+        <View style={{ flex: 1, justifyContent: "center" }}>
           <Image
             source={require("../../assets/sponge.gif")}
             style={{ width: "100%", height: 400 }}
